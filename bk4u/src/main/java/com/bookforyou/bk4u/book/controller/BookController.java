040ce54bc914746dd03be7492f89c9c97332e7df
yungc97 <2__nd@naver.com>
2021-07-27 22:01:11 +0900

    PageInfo 추가

--- bk4u/src/main/java/com/bookforyou/bk4u/book/controller/BookController.java
+++ bk4u/src/main/java/com/bookforyou/bk4u/book/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bookforyou.bk4u.book.model.service.BookService;
 import com.bookforyou.bk4u.book.model.vo.Book;
@@ -23,5 +25,10 @@ public class BookController {
 		System.out.println(bList);
 		model.addAttribute("bList", "bList");
 		return "book/adminBookList";
+	}
+	
+	@RequestMapping("search.bk")
+	public String searchBook(ModelAndView mv, @RequestParam(value="currentPage", defaultValue="1") int currentPage, String condition, String keyword) {
+		return "book/bookSearchList.jsp";
 	}
 }
