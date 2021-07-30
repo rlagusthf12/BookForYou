37c6128364fe0c5bb8506ba1f738c0f63affa9a9
daeun lee <79685920+daeuun@users.noreply.github.com>
2021-07-30 20:05:18 +0900

    docs : 정기구독 vo 추가

---
+++ bk4u/src/main/java/com/bookforyou/bk4u/subscription/model/vo/Subscription.java
@@ -0,0 +1,34 @@
+package com.bookforyou.bk4u.subscription.model.vo;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter
+@ToString
+public class Subscription {
+	
+	public int subscNo;
+	public int memNo;
+	public int payNo;
+	public int couponIssueNo;
+	public String subscName;
+	public String subscPeriod;
+	public String subscPrice;
+	public Date subscSdate;
+	public String subscEndDate;
+	public int subscDelDate;
+	public String subscStatus;
+	public String subscReceiver;
+	public String subscPost;
+	public String subscAddress;
+	public String addressRetail;
+	public String addressRef;
+	public int subscPhone;
+	public String deliveryRequest;
+
+}
