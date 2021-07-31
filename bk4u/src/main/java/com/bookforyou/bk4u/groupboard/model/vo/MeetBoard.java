6e468400f560ff0df60dc5b8a89258727bc70a67
1ovety <qazppo@naver.com>
2021-07-31 16:44:37 +0900

    Update MeetBoard class 업데이트 group-mapper 세미콜론 삭제

--- bk4u/src/main/java/com/bookforyou/bk4u/groupboard/model/vo/MeetBoard.java
+++ bk4u/src/main/java/com/bookforyou/bk4u/groupboard/model/vo/MeetBoard.java
@@ -1,5 +1,26 @@
 package com.bookforyou.bk4u.groupboard.model.vo;
 
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter @Setter
+@ToString
 public class MeetBoard {
 
+	private int mBoardNo;
+	private int groupBoardNo;
+	private int memNo;
+	private String mBoardContent;
+	private Date mBoardCreateDate;
+	private String mBoardStatus;
+	private String groupLevel;
+	private String upFileOrigin;
+	private String upFileUrl;
+
+	
 }
