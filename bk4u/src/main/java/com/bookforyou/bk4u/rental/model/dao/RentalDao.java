7706b06f5cdbf5e3028bb497033f81be4c04eb1c
yungc97 <2__nd@naver.com>
2021-08-03 17:33:20 +0900

    docs: rental 패키지 수정

---
+++ bk4u/src/main/java/com/bookforyou/bk4u/rental/model/dao/RentalDao.java
@@ -0,0 +1,35 @@
+package com.bookforyou.bk4u.rental.model.dao;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bookforyou.bk4u.common.model.vo.PageInfo;
+import com.bookforyou.bk4u.rental.model.vo.Rental;
+
+@Repository
+public class RentalDao {
+
+	/*
+	 * [사용자] 대여 내역 개수 조회 (연지)
+	 */
+	public int selectRentalCount(SqlSessionTemplate sqlSession, int memNo) {
+		return sqlSession.selectOne("rentalMapper.selectRetalCount", memNo);
+	}
+
+	/*
+	 * [사용자] 대여 내역 리스트 조회 (연지)
+	 */
+	public ArrayList<Rental> selectRentalList(SqlSessionTemplate sqlSession, PageInfo pi, int memNo) {
+		return (ArrayList)sqlSession.selectList("rentalMapper.selectRentalList", pi, memNo);
+	}
+
+	/*
+	 * [사용자] 대여 내역 조회 (연지)
+	 */
+	public Rental selectRental(SqlSessionTemplate sqlSession, int rentalNo) {
+		return sqlSession.selectOne("rentalMapper.selectRental", rentalNo);
+	}
+
+}
