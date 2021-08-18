c0e30763b6566b4d0b9267bf54d1e18f1492f565
Hjin-Y <76461035+Hjin-Y@users.noreply.github.com>
2021-08-18 19:59:37 +0900

    지점별 보유 도서 추가 기능

--- bk4u/src/main/java/com/bookforyou/bk4u/book/model/dao/BookDao.java
+++ bk4u/src/main/java/com/bookforyou/bk4u/book/model/dao/BookDao.java
@@ -230,4 +230,20 @@ public class BookDao {
 	public ArrayList<Book> selectMainBookRecommandToAll(SqlSessionTemplate sqlSession) {
 		return (ArrayList)sqlSession.selectList("bookMapper.selectMainBookRecommandToAll");
 	}
+
+	/**
+	 * [관리자] 도서 목록 조회 - 지점별 도서 추가를 위해서 (한진)
+	 */
+	public ArrayList<Book> selectBookListForStore(SqlSessionTemplate sqlSession, PageInfo pi, HashMap<String, String> filter) {
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		return (ArrayList)sqlSession.selectList("bookMapper.selectAdminBookList", filter, rowBounds);
+	}
+
+	/**
+	 * [관리자] 지점별 도서 추가 (한진)
+	 */
+	public int insertBookForStore(SqlSessionTemplate sqlSession, HashMap<String, String> map) {
+		return sqlSession.insert("bookMapper.insertBookForStore", map);
+	}
 }
